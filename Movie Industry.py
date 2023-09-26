56d659cbab43fad9dd4368563c62e9b846c0b672
Nuzhat Zahan <112464499+swarba015@users.noreply.github.com>
2023-09-25 23:23:16 -0400

    Movie Industry.py

--- Movie Industry.py
+++ Movie Industry.py
@@ -1,149 +1,65 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[19]:
-
-
-#Import libraries
+#Import Library
 import pandas as pd
 import numpy as np
 import seaborn as sns
-import matplotlib 
 import matplotlib.pyplot as plt
-plt.style.use('ggplot')
-from matplotlib.pyplot import figure
 
-get_ipython().run_line_magic('matplotlib', 'inline')
-matplotlib.rcParams['figure.figsize'] = (12,8) #adjusts the configuration of the plot we will create
-
+# Import and read data
+df = pd.read_csv(r"C:\Users\nuzha\Downloads\movies.csv")  # Adjust the path as needed
+df.head(5)
 
 
-# In[23]:
+# Drop NA values
+df.dropna(subset=['rating', 'budget', 'gross'], inplace=True)
 
 
-#Import and read data
-df = pd.read_csv("C:\\Users\\Towfiq\\Documents\\movies.csv")
-df.head()
+# Change data types of votes, budget, and gross columns
+df = df.astype({"votes": "int64", "budget": "int64", "gross": "int64"})
 
+# Drop company duplicates
+df['company'].drop_duplicates(inplace=True)
 
-# In[24]:
+# Order by gross
+df.sort_values(by=['gross'], inplace=True, ascending=False)
 
-
-#Shape of data set
-df.shape
-
-
-# In[25]:
-
-
-# Data Types of columns
-
-print(df.dtypes)
-
-
-# In[21]:
-
-
-#Finding missing data
-for col in df.columns:
-    missing_data = np.mean(df[col].isnull())
-    print('{} - {}%'.format(col, round(missing_data*100)))
-
-
-# In[26]:
-
-
-#Cleaning data
-df2x = df.dropna(axis=0, subset=['rating','budget','gross'])
-df2x.info()
-
-
-# In[33]:
-
-
-#Changing data type of votes
-df2x = df2x.astype({"votes" :np.dtype("int64"), "budget" : np.dtype("int64"), "gross" : np.dtype("int64")})
-df2x.info()
-
-
-# In[36]:
-
-
-df2x["company"] = df2x["company"].drop_duplicates().sort_values()
-df2x.info()
-
-
-# In[42]:
-
-
-#Ordering data to understand better
-
-df2x_sorting_gross_values = df2x.sort_values(by=['gross'], inplace=False, ascending=False)
-df2x_sorting_gross_values.head()
-
-
-# In[43]:
-
-
-# scatterplot with budget vs gross 
-plt.scatter(x=df2x['budget'], y=df2x['gross'])
+# Scatter plot with budget vs gross
+plt.scatter(x=df['budget'], y=df['gross'])
 plt.title('Budget vs Gross Revenue')
-plt.ylim([0,2*10**9])
+plt.ylim([0, 2*10**9])
 plt.xlabel('Gross Revenue')
 plt.ylabel('Budget for Films')
+plt.show()
 
 
-# In[47]:
+# Regplot budget vs gross
+sns.regplot(x='budget', y='gross', data=df, scatter_kws={'color':'red'}, line_kws={'color':'blue'})
+plt.ylim([0, 2*10**9])
+plt.show()
 
 
-#budget vs gross (regplot)
-sns.regplot(x='budget', y='gross', data = df2x, scatter_kws={'color':'red'}, line_kws={'color':'blue'})
-plt.ylim([0,2*10**9])
-
-
-# In[49]:
-
-
-# Tho correlation table looks as follows.
-df.corr(method = 'pearson')
-
-
-# In[51]:
-
-
-# plotting the Heatmap
-
+# Heatmap of correlation matrix
 corr_mat = df.corr(method='pearson')
 sns.heatmap(corr_mat, annot=True)
-plt.title('Correlation Metric for Numberic features')
+plt.title('Correlation Metric for Numeric Features')
 plt.xlabel('Movie Features')
 plt.ylabel('Movie Features')
+plt.show()
 
 
-# In[52]:
-
-
-#Numerization of Object Data Types
-df_numerized = df2x
+# Numerization of Object Data Types
+df_numerized = df.copy()
 for col_name in df_numerized.columns:
-    if(df_numerized[col_name].dtype == 'object'):
+    if df_numerized[col_name].dtype == 'object':
         df_numerized[col_name] = df_numerized[col_name].astype('category')
         df_numerized[col_name] = df_numerized[col_name].cat.codes
+
 df_numerized.head()
 
 
-# In[53]:
-
-
+## Heatmap of numerized correlation matrix
 correlation_matrix = df_numerized.corr(method='pearson')
 sns.heatmap(correlation_matrix, annot=True)
-plt.title('Correlation Metric for Numberic features')
+plt.title('Correlation Metric for Numeric Features')
 plt.xlabel('Movie Features')
 plt.ylabel('Movie Features')
-
-
-# In[ ]:
-
-
-
-
+plt.show()
